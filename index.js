65e835b0ad8d6a53d31a526b760a23514073fa02
github username <cmyerskeitt@gmail.com>
2022-10-30 13:06:39 -0400

    refactor

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// function filter_list(l) {
+//     let numbersOnly = []
+//    for (let i = 0; i < l.length; i++){
+//      if (Number.isInteger(l[i]) == true){
+//        numbersOnly.push(l[i])
+//       }
+//      }
+//    return numbersOnly
+// }
+
 function filter_list(l) {
-    let numbersOnly = []
-   for (let i = 0; i < l.length; i++){
-     if (Number.isInteger(l[i]) == true){
-       numbersOnly.push(l[i])
-      }
-     }
-   return numbersOnly
+    return l.filter(Number.isInteger);
 }
